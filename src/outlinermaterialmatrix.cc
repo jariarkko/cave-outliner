ea99b70c892d9ffebc8ea53d3e280440f306a5b3
Jari Arkko <jari.arkko@piuha.net>
2021-07-17 15:03:45 +0300

    more features

--- src/outlinermaterialmatrix.cc
+++ src/outlinermaterialmatrix.cc
@@ -77,7 +77,7 @@ MaterialMatrix::getMaterialMatrix(unsigned int xIndex,
 unsigned int
 MaterialMatrix::count(void) {
   unsigned int theCount = 0;
-  for (unsigned int i = 0; i <= nChars; i++) {
+  for (unsigned int i = 0; i < nChars; i++) {
     assert(i < nChars);
     unsigned char theChar = bitMatrix[i];
     if (theChar != 0) {
