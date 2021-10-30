d0267e7d91c8c21cf0208f601494933ba79535cb
Jari Arkko <jari.arkko@piuha.net>
2021-10-30 15:12:45 +0300

    working on #7

--- doc/markdowncleanup.sh
+++ doc/markdowncleanup.sh
@@ -27,5 +27,7 @@ cat $INPUT |
     sed 's/.img src="doxygen.svg" class="footer" width="104" height="31" alt="doxygen" ../Doxygen/g' |
     sed 's/.List of all//g' |
     sed 's/members..class_outliner_math-members.html.//g' |
-    sed 's@<a href="class_outliner_triangle3_d.html" class="el">OutlinerTriangle3D</a>@[OutlinerTriangle3D](https://github.com/jariarkko/cave-outliner/blob/master/doc/class_outliner_triangle3_d.md)@g' |
+    sed 's@href="class@href="https://github.com/jariarkko/cave-outliner/blob/master/doc/class@g' |
+    sed 's@.html@.md@g' |
+#    sed 's@<a href="class_outliner_triangle3_d.html" class="el">OutlinerTriangle3D</a>@[OutlinerTriangle3D](https://github.com/jariarkko/cave-outliner/blob/master/doc/class_outliner_triangle3_d.md)@g' |
     cat  > $OUTPUT
